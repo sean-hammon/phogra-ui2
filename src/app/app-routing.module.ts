324f2a91bd761b8b4e5cbfba9807c767f7b9ede1
Sean Hammon <git@sean-hammon.com>
2017-07-13 09:34:30 -0600

    viewport: add a viewport component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewportComponent } from './viewport/viewport.component'
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: ViewportComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewportComponent } from './viewport/viewport.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewportComponent
   ],
   imports: [
     BrowserModule,
