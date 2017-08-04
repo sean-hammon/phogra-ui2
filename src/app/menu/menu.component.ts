8af75c6100c431cdbc1b568f535458b350ba734a
Sean Hammon <git@sean-hammon.com>
2017-08-04 15:35:21 -0600

    menu.component: hook up menu visibility to the ngrx store.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from "@ngrx/store";
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.sass']
-})
-export class MenuComponent implements OnInit {
-
-    constructor() {
+    styleUrls: ['./menu.component.sass'],
+    host: {
+        '[class.open]': 'menuOpen'
     }
+})
+export class MenuComponent {
 
-    ngOnInit() {
+    menuOpen: boolean;
+
+    constructor(private store: Store<any>)
+    {
+        store.select('menuState')
+            .subscribe(state => {
+                this.menuOpen = state.menuOpen;
+            });
+
     }
 
 }
