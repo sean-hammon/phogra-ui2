c544997e9a97e1b4e1e3025007e0b108e02e74ed
Sean Hammon <git@sean-hammon.com>
2017-08-07 14:29:41 -0600

    menu: wire up state service to slide menu in and out.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from "../state.service";
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.sass']
+    styleUrls: ['./menu.component.sass'],
+    host: {
+        '[class.open]': "state.menuOpen"
+    }
 })
 export class MenuComponent implements OnInit {
 
-    constructor() {
-    }
+    constructor(
+        private state: StateService
+	) { }
 
     ngOnInit() {
     }
