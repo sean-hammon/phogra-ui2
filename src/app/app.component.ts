861f0a553a3230a414bdbad322b513b81c94ec7b
Sean Hammon <git@sean-hammon.com>
2017-08-07 12:30:51 -0600

    app.component: hook up the page title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-    title = 'app';
+export class AppComponent implements OnInit {
+
+    pageTitle = 'Sean Hammon Photography';
+
+    constructor(
+        private title: Title
+    ) {}
+
+    ngOnInit() {
+        this.title.setTitle(this.pageTitle);
+    }
 }
