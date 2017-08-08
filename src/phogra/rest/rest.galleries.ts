cc5999ad47e227679e82a280967a2466dded167a
Sean Hammon <git@sean-hammon.com>
2017-08-08 16:51:35 -0600

    rest.galleries: interface definitions for the shape of the galleries API response.

---
+++ src/phogra/rest/rest.galleries.ts
@@ -0,0 +1,24 @@
+export interface IRestGalleryRelations {
+    children?: any;
+    photos?: any;
+}
+
+export interface IRestGalleryAttributes {
+    parent_id: string;
+    title: string;
+    slug: string;
+    node: string;
+    description: string;
+    featured: number;
+    restricted: boolean;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface IRestGalleryData {
+    type: string;
+    id: string;
+    attributes: IRestGalleryAttributes;
+    relationships: IRestGalleryRelations;
+    links: any;
+}
