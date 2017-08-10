8a85b175bb354b30ae88902e60250289e90c3c98
Sean Hammon <git@sean-hammon.com>
2017-08-10 10:54:23 -0600

    app.state: add actions/reducers for GalleryEffects.

--- src/app/store/app.actions.ts
+++ src/app/store/app.actions.ts
@@ -6,3 +6,6 @@ export interface ReducerAction
 
 export const TOGGLE_MENU = 'TOGGLE_MENU';
 
+export const GET_GALLERIES = 'GET_GALLERIES';
+export const GET_GALLERIES_SUCCESS = 'GET_GALLERIES_SUCCESS';
+export const GET_GALLERIES_ERROR = 'GET_GALLERIES_ERROR';
--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -13,6 +13,23 @@ export const appReducer = (
                 menuOpen: !state.menuOpen
             });
 
+        case Actions.GET_GALLERIES:
+            return Object.assign({}, state, {
+                loading: true,
+                error: null
+            });
+
+        case Actions.GET_GALLERIES_SUCCESS:
+            return Object.assign({}, state, {
+                galleries: action.payload
+            });
+
+        case Actions.GET_GALLERIES_ERROR:
+            return Object.assign({}, state, {
+                loading: false,
+                error: "Error"
+            });
+
         default:
             return state;
     }
