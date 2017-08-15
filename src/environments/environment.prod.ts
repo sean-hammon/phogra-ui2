f3c863db79680d3b52c741738f8acd27bb52c177
Sean Hammon <git@sean-hammon.com>
2017-08-15 11:58:35 -0600

    environments: add API URL to environment files. Undo currently hard coded value.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+    license: 'Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)',
+    apiBase: 'http://phogra.sean-hammon.com',
+    jwtCookie: 'X-PHOGRA-JWT'
 };
--- src/phogra/galleries/gallery.service.ts
+++ src/phogra/galleries/gallery.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { Gallery } from "./gallery";
 import { Observable } from "rxjs/Observable";
+import { environment } from "../../environments/environment";
 import "rxjs/add/operator/map";
 
 @Injectable()
 export class GalleryService {
 
-    private apiUrl = 'http://papi.dev';
     private apiEndpoint = "/galleries";
 
     constructor (
@@ -16,7 +16,7 @@ export class GalleryService {
 
     fetchGalleries(): Observable<Gallery[]> {
 
-        return this.http.get(this.apiUrl + this.apiEndpoint)
+        return this.http.get(environment.apiBase + this.apiEndpoint)
             .map(response => {
                 let data = response.json().data;
                 return data.map(item => Gallery.transformRest(item));
