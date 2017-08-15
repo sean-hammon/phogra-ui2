b0f95b232cbdbab6e9590cf6bff2816d27f9a233
Sean Hammon <git@sean-hammon.com>
2017-08-15 10:04:42 -0600

    app.component: Move gallery initialization to app component.
    Routing is going to change an this is a better place for it anyway.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Store } from "@ngrx/store";
+import { AppState } from "./store/app.state";
+import { GET_GALLERIES } from "./store/app.actions";
 
 @Component({
     selector: 'app-root',
@@ -11,10 +14,14 @@ export class AppComponent implements OnInit {
     pageTitle = 'Sean Hammon Photography';
 
     constructor(
+        private store: Store<AppState>,
         private title: Title
     ) {}
 
     ngOnInit() {
         this.title.setTitle(this.pageTitle);
+        this.store.dispatch({
+            type: GET_GALLERIES
+        });
     }
 }
