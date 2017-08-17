46c6bdef2ca7ad64c4d69c003a300a94fa1dd1a9
Sean Hammon <git@sean-hammon.com>
2017-08-17 15:52:21 -0600

    routing-module: add gallery route

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhotoComponent } from '../photo/photo.component'
+import { RouteResolver } from "./route.resolver";
+import { GalleryComponent } from "../gallery/gallery.component";
 
 const routes: Routes = [
+    {
+        path: 'gallery/:slug',
+        children: [
+            {
+                path: "**",
+                component: GalleryComponent,
+                resolve: {
+                    success: RouteResolver
+                }
+            }
+        ]
+    },
     {
         path: '',
         pathMatch: 'full',
