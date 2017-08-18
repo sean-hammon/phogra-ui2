7805d84ee7e90d93ac902b73425e35ecbfee17ce
Sean Hammon <git@sean-hammon.com>
2017-08-18 10:10:41 -0600

    gallery.component: four spaces, add host class.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.sass']
+    selector: 'app-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.sass'],
+    host: {
+        'class': 'full-frame'
+    }
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
