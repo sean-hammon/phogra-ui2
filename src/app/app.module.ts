b0b1328fa2e43524c5a6bfb0b3e949b4149b2dd7
Sean Hammon <git@sean-hammon.com>
2017-08-21 16:16:43 -0600

    app.module: import BrowserAnimationsModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { StoreModule} from '@ngrx/store';
 
