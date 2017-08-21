29f2445ca8ede6d2325232b7346ebe7ab1131cfe
Sean Hammon <git@sean-hammon.com>
2017-08-21 14:25:24 -0600

    Four spaces please.

--- src/app/widgets/spinner/spinner.component.ts
+++ src/app/widgets/spinner/spinner.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.sass']
+    selector: 'app-spinner',
+    templateUrl: './spinner.component.html',
+    styleUrls: ['./spinner.component.sass']
 })
 export class SpinnerComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
