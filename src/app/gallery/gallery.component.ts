878bb1dae8a0a982c90aec25f4ec6ad24c48c71a
Sean Hammon <git@sean-hammon.com>
2017-08-22 15:04:09 -0600

    gallery.component: listen for state changes indicating thumbs are ready.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,21 +1,32 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {Component, HostBinding } from '@angular/core';
+import { Store } from "@ngrx/store";
+import { loadComplete, thumbsState } from "../store/app.state";
+import { Photo } from "../../phogra/photos/photo";
 
 @Component({
     selector: 'app-gallery',
     templateUrl: './gallery.component.html',
     styleUrls: ['./gallery.component.sass']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
+
+    thumbs: Photo[];
+    thumbs_loaded: boolean;
 
     @HostBinding('class')
     public get getClass() {
         return 'full-frame';
     }
 
-    constructor() {
-    }
+    constructor(
+        private store: Store<any>
+    ) {
+        this.thumbs_loaded = false;
+        store.select(thumbsState)
+            .subscribe(thumbs => this.thumbs = thumbs);
 
-    ngOnInit() {
+        store.select(loadComplete)
+            .subscribe(() => this.thumbs_loaded = true)
     }
 
 }
