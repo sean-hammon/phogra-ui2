f42c4834d0873d8860168a34f6bb2c8a0a7a41db
Sean Hammon <git@sean-hammon.com>
2017-08-29 16:04:24 -0600

    thumb.component: use gallery object to build correct photo link.

--- src/app/gallery/thumb/thumb.component.ts
+++ src/app/gallery/thumb/thumb.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Photo } from "../../../phogra/photos/photo";
+import { Gallery } from "../../../phogra/galleries/gallery";
 
 @Component({
     selector: 'app-thumb',
     templateUrl: './thumb.component.html',
     styleUrls: ['./thumb.component.sass']
 })
-export class ThumbComponent {
+export class ThumbComponent implements OnInit {
 
     @Input()
     thumb: Photo;
+
+    @Input()
+    gallery: Gallery;
+
+    thumb_link: string;
+
+    ngOnInit() {
+        this.thumb_link = `${this.thumb.links.ui}/in/${this.gallery.path}/${this.thumb.id}`;
+    }
 
 }
