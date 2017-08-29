9e783e8ce8a934fa91518462d50cd3f3c4a0f3c9
Sean Hammon <git@sean-hammon.com>
2017-08-29 14:53:34 -0600

    top-bar.component: keep display data updated based on route/current object.

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -3,7 +3,9 @@ import { Router, NavigationStart } from '@angular/router';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/skip';
 import { Store } from '@ngrx/store';
-import { currentPhoto } from '../store/app.state';
+import { currentGallery, currentPhoto } from '../store/app.state';
+import { Photo } from "../../phogra/photos/photo";
+import { Gallery } from "../../phogra/galleries/gallery";
 
 @Component({
     selector: 'app-topbar',
@@ -13,16 +15,28 @@ import { currentPhoto } from '../store/app.state';
 export class TopBarComponent implements OnInit {
 
     page_title: string;
-    photo_description: string;
+    description: string;
+    current_view: string;
+    total_photos: number;
 
     constructor(
         private store: Store<any>,
         private router: Router
     ) {
+        this.current_view = 'photo';
+        this.total_photos = 0;
+
         store.select(currentPhoto).skip(1)
             .subscribe(photo => {
-                this.page_title = photo.title;
-                this.photo_description = photo.short_desc;
+                if (this.current_view === 'photo') {
+                    this.updateWithPhotoInfo(photo);
+                }
+            });
+        store.select(currentGallery).skip(1)
+            .subscribe(gallery => {
+                if (this.current_view === 'gallery') {
+                    this.updateWithGalleryInfo(gallery);
+                }
             });
 
     }
@@ -31,10 +45,22 @@ export class TopBarComponent implements OnInit {
     ngOnInit() {
         this.router.events
             .filter(event => event instanceof NavigationStart)
-            .subscribe(() => {
+            .subscribe((event: NavigationStart) => {
+                this.current_view = event.url.split('/')[1] || 'photo';
                 this.page_title = '';
-                this.photo_description = '';
+                this.description = '';
             });
+    }
+
+
+    updateWithPhotoInfo(photo: Photo) {
+        this.page_title = photo.title;
+        this.description = photo.short_desc;
+    }
+
+
+    updateWithGalleryInfo(gallery: Gallery) {
+        this.page_title = gallery.title;
     }
 
 }
