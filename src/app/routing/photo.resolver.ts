fa8aa7c7590e217ec152eb85a06118ce9f8c5687
Sean Hammon <seanhamm@utah.gov>
2017-09-19 10:22:10 -0600

    photo.resolver: put requisite logic from AppResolver into PhotoResolver.

--- src/app/routing/photo.resolver.ts
+++ src/app/routing/photo.resolver.ts
@@ -1,16 +1,77 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import { GalleryService } from '../../phogra/galleries/gallery.service';
+import { GalleryProvider } from '../../phogra/galleries/gallery.provider';
+import { Gallery } from '../../phogra/galleries/gallery';
+import { PhotoService } from '../../phogra/photos/photo.service';
+import { PhotoProvider } from '../../phogra/photos/photo.provider';
+import { Photo } from '../../phogra/photos/photo';
+import { PRELOAD_BEGIN, PRELOAD_COMPLETE } from '../store/app.actions';
 
 @Injectable()
 export class PhotoResolver implements Resolve<boolean> {
 
-    constructor() {
-    }
+    constructor(
+        private store: Store<any>,
+        private galleries: GalleryProvider,
+        private galleryApi: GalleryService,
+        private photoApi: PhotoService,
+        private photos: PhotoProvider,
+    ) { }
 
 
     resolve(route: ActivatedRouteSnapshot): Observable<any> {
-        console.log('gallery resolver');
-        return Observable.of(true);
+
+        const home_route =  route.url.length === 0;
+        let photo_id = '',
+            gallery_slug = '';
+
+        //  Make sure the spinner starts with each route change.
+        this.store.dispatch({
+            type: PRELOAD_BEGIN
+        });
+
+        let gallery: Gallery;
+        if (home_route) {
+
+            gallery = this.galleries.setDefaultGallery();
+
+        } else {
+
+            //  If this isn't the home route, we don't necessarily know which
+            //  gallery we are dealing with (external link directly to the photo URL.)
+            photo_id = route.url.pop().path;
+            gallery_slug = route.url.pop().path;
+            gallery = this.galleries.setBySlug(gallery_slug);
+
+        }
+
+        return this.galleryApi.fetchGalleryPhotos(gallery)
+            .switchMap((photos): Observable<Photo> => {
+                this.photos.setPhotos(photos);
+
+                let photo: Photo;
+                if (home_route) {
+                    photo = this.photos.random();
+                } else {
+                    photo = this.photos.fetchById(photo_id);
+                }
+
+                return this.photoApi.preloadFile(photo, 'hifi');
+
+            })
+            .map(photo => {
+                this.store.dispatch({
+                    type: PRELOAD_COMPLETE
+                });
+
+                return photo;
+            })
+            .first();
+
     }
+
 }
