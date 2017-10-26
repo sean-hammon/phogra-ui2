53f2f798c46ae05ea78680a29bfb328b80e6e830
Sean Hammon <seanhamm@utah.gov>
2017-10-26 11:22:47 -0600

    gallery.resolver: refactor to listen for preload event.

--- src/app/routing/gallery.resolver.ts
+++ src/app/routing/gallery.resolver.ts
@@ -4,7 +4,10 @@ import { Observable } from 'rxjs/Observable';
 import { GalleryProvider } from '../../phogra/galleries/gallery.provider';
 import { GalleryService } from '../../phogra/galleries/gallery.service';
 import { Store } from '@ngrx/store';
-import { AppPreloadBeginAction, ThumbsInitializePageAction } from '../store/app.actions';
+import {
+    AppPreloadBeginAction, ThumbsAppendAction, ThumbsInitializePageAction,
+    ThumbsResetAction
+} from '../store/app.actions';
 import { PhotoProvider } from '../../phogra/photos/photo.provider';
 import { PhotoService } from '../../phogra/photos/photo.service';
 import { Photo } from '../../phogra/photos/photo';
@@ -33,6 +36,8 @@ export class GalleryResolver implements Resolve<Photo[]> {
         const gallery_id = route.url.pop().path;
         const gallery = this.galleries.setById(gallery_id);
 
+        this.store.dispatch(new ThumbsResetAction());
+
         return this.galleryApi.fetchGalleryPhotos(gallery)
             .flatMap((photos: Photo[]) => {
 
@@ -51,9 +56,11 @@ export class GalleryResolver implements Resolve<Photo[]> {
                     this.store.dispatch(new ThumbsInitializePageAction(gallery_id));
                 }
 
-                const thumbs = this.ThumbCalculator.fetchPageRange(0, current_page);
-                return this.photoApi.preloadThumbs(thumbs);
+                return this.ThumbCalculator.fetchPageRange(0, current_page);
 
+            })
+            .do(thumbs => {
+                this.store.dispatch(new ThumbsAppendAction(thumbs));
             })
             .first();
 
