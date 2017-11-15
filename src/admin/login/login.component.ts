ceb950548df9826a716484cac4a0876e6244719f
Sean Hammon <seanhamm@utah.gov>
2017-11-15 16:45:01 -0700

    login: update onSubmit to dispatch an action to the store.

--- src/admin/login/login.component.ts
+++ src/admin/login/login.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../user/user.model';
-import { NgForm } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IUserLogin, User } from '../../phogra/user/user.model';
+import { LoginAction } from '../store/admin.actions';
+import { Store } from '@ngrx/store';
+import { AdminState, apiErrorState, userState } from '../store/admin.state';
+import 'rxjs/add/operator/skip';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.sass']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    busy: boolean;
+    loginError: string;
+
+    constructor(
+        private store: Store<AdminState>
+    ) {
+        this.busy = false;
     }
 
     ngOnInit() {
+    ngOnDestroy() {
     }
 
-    onSubmit({value, valid}: {value: User, valid: boolean}) {
-        console.log(value, valid );
+    onSubmit({value, valid}: {value: IUserLogin, valid: boolean}) {
+
+        if (valid) {
+            this.busy = true;
+            this.store.dispatch(new LoginAction(value));
+        }
+
     }
 
 }
