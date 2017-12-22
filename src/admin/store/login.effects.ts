a8128679c03fe0f2f2d86c0e04154b4098a5c295
Sean Hammon <seanhamm@utah.gov>
2017-12-22 13:22:50 -0700

    admin/login.effects: add effect to route to dashboard on LOGIN_SUCCESS.

--- src/admin/store/login.effects.ts
+++ src/admin/store/login.effects.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../../phogra/auth/auth.service';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
-import { AdminActions, LoginErrorAction, LoginSuccessAction } from './admin.actions';
+import { AdminActions, LoginErrorAction, LoginSuccessAction, NoOpAction } from './admin.actions';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class LoginEffects {
 
     constructor (
         private actions$: Actions,
-        private auth: AuthService
+        private auth: AuthService,
+        private router: Router
     ) {}
 
 
@@ -27,4 +30,22 @@ export class LoginEffects {
             }
         );
 
+    /**
+     * After the store has been updated, navigate to the appropriate
+     * user dashboard.
+     *
+     * @type {Observable<any>}
+     */
+    @Effect()
+    doNav$ = this.actions$
+        .ofType(AdminActions.LOGIN_SUCCESS)
+        .map(toPayload)
+        .map(
+            user => {
+                this.router.navigate(['/dashboard']);
+
+                return new NoOpAction();
+            }
+        )
+        .catch((err) => Observable.of(new LoginErrorAction(err)));
 }
