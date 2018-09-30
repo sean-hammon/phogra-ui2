01ea8514e9aceac75dc879af3d475baf49a3d173
Sean Hammon <git@sean-hammon.com>
2018-09-30 15:48:24 -0600

    gallery: dummy up some data until we're ready to connect to API.

--- src/app/gallery/gallery/gallery.component.ts
+++ src/app/gallery/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Gallery } from '../../../phogra/galleries/gallery';
 
 @Component({
     selector: 'app-gallery',
@@ -7,8 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+    gallery: Gallery;
+    children: Gallery[];
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.gallery = {
+            id: 'xyz',
+            parent_id: 'foo',
+            title: '__root',
+            node: '',
+            slug: '__root',
+            featured: 0,
+            restricted: false,
+            links: {
+                self: '',
+                photos: '',
+                children: '',
+                ui: ''
+            },
+            relationships: {
+                children: []
+            },
+            created_at: null,
+            updated_at: null
+        };
+
+        this.children = [
+            {
+                id: 'xyz',
+                parent_id: 'foo',
+                title: 'Travel',
+                node: '',
+                slug: '__root',
+                featured: 0,
+                thumb: 'http://phogra.sean-hammon.com/photos/mBAMoB',
+                restricted: false,
+                links: {
+                    self: '',
+                    photos: '',
+                    children: '',
+                    ui: ''
+                },
+                relationships: {
+                    children: []
+                },
+                created_at: null,
+                updated_at: null
+            },
+            {
+                id: 'xyz',
+                parent_id: 'foo',
+                title: 'Sport',
+                node: '',
+                slug: '__root',
+                featured: 0,
+                thumb: 'http://phogra.sean-hammon.com/photos/Vpv2Wp',
+                restricted: false,
+                links: {
+                    self: '',
+                    photos: '',
+                    children: '',
+                    ui: ''
+                },
+                relationships: {
+                    children: []
+                },
+                created_at: null,
+                updated_at: null
+            }
+        ];
+    }
 
 }
