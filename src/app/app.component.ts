48ea7b8409ab745048543007fe21ddeaec0c28ec
Sean Hammon <sean@sean-hammon.com>
2018-10-20 15:49:08 -0600

    app.component: dispatch event to load galleries.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
+import { PhograState } from '../phogra/store';
+import { LoadGalleries } from '../phogra/store/galleries';
+
 
 @Component({
     selector: 'app-root',
@@ -11,10 +15,12 @@ export class AppComponent implements OnInit {
     pageTitle = 'Sean Hammon Photography';
 
     constructor(
-        private title: Title
-    ) { }
+        private title: Title,
+        private store: Store<PhograState>
+    ) {}
 
     ngOnInit() {
         this.title.setTitle(this.pageTitle);
+        this.store.dispatch(new LoadGalleries());
     }
 }
