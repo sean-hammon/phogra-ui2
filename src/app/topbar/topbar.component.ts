8fed3f25b31253014287163fd153a88188bfa393
Sean Hammon <git@sean-hammon.com>
2018-11-18 12:02:09 -0700

    topbar: wire up to use gallery provider to display gallery title.

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Gallery } from '../../phogra/galleries/gallery';
+import { Photo } from '../../phogra/photos/photo';
+import { GalleryProvider } from '../gallery/gallery.provider';
 
 @Component({
     selector: 'app-topbar',
@@ -7,10 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-    constructor() {
-    }
+    public onDisplay$: Observable<Gallery|Photo>;
+
+    constructor(
+        private galleries: GalleryProvider,
+        private activatedRoute: ActivatedRoute
+    ) {}
 
     ngOnInit() {
+        this.activatedRoute.url.subscribe((segments) => {
+            console.log(segments[0].path);
+            if (segments[0].path == '' || segments[0].path == 'gallery') {
+                this.onDisplay$ = this.galleries.currentGallery();
+            }
+        });
     }
 
 }
