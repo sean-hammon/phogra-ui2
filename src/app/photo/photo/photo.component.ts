6a7ff1aa8b652bbf88ec7e6cf4a44961668af0e9
Sean Hammon <git@sean-hammon.com>
2018-12-17 21:42:08 -0700

    photo: sketch in theoretical animations.

--- src/app/photo/photo/photo.component.ts
+++ src/app/photo/photo/photo.component.ts
@@ -1,13 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-photo',
     templateUrl: './photo.component.html',
-    styleUrls: ['./photo.component.sass']
+    styleUrls: ['./photo.component.sass'],
+    animations: [
+        trigger('crossFade', [
+            state('showing', style({
+                opacity: 1
+            })),
+            state('hidden', style({
+                opacity: 0
+            })),
+            transition('hidden <=> showing', [
+                animate('0.75s')
+            ])
+        ]),
+    ]
 })
 export class PhotoComponent implements OnInit {
 
     @HostBinding('class.full-frame') true;
+
+    svgVisible = false;
+    photoVisible = false;
 
     constructor() {
     }
